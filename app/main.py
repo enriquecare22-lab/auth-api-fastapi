a21ccf80a6bfbdf2dd257887ebd8c5903eb859cb
Enrique <enriquecare22@gmail.com>
2026-04-26 20:04:58 -0600

    fix: correct imports for engine and database initialization

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from app.api.v1.routes import auth
 
 from app.models.user import Base
-from app.db.session import settings
+from app.db.session import engine
 
+# Crear tablas (solo desarrollo)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Registrar rutas
 app.include_router(auth.router)
