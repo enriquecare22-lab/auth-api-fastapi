d889b3702ef7939c7393ae92f9ab491b37687ca5
Enrique <enriquecare22@gmail.com>
2026-04-29 16:39:06 -0600

    fix(security): correct JWT encoding, expiration and token validation

--- app/core/security.py
+++ app/core/security.py
@@ -46,22 +46,31 @@ def create_refresh_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
-    to_encode.update({"exp": expire, "type": "access"})
+    to_encode.update({"exp": expire, "type": "refresh"})
 
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
-def decode_token(token: str):
+def decode_token(token: str, expected_type: str = "access"):
     """
-    Decodifica un JWT y retorna el payload
-    - Si el token es valido: devuelve datos (ej: email)
-    - Si es invalido: retorna NONE
+    Decodifica un JWT y valida que sea del tipo corecto (access o refresh)
     """
     try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-        )
-        print(payload)
+        # Intenta descifrar el token usando la llave secreta y el algoritmo (ej. HS256)
+        # Si el token expiró o la firma fue alterada, lanzará una excepción JWTError.
+
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+
+        # Validación de Seguridad Crítica:
+        # Verifica que el 'tipo' de token en el payload coincida con lo esperado.
+        # Esto evita que usen un 'refresh_token' para acceder a rutas de datos (access).
+        if payload.get("type") != expected_type:
+            return None
+
+            # Si todo es correcto, devuelve los datos contenidos (sub, exp, type, etc.)
         return payload
+
     except JWTError:
+        # Si el token es invalido, mal formado o ha expirado,
+        # capturamos el error y retornamos None para denegar el acceso
         return None
